3361398843e8eb63cfc7d0d07f7cc0ef1c988b19
Ivan Okhrimenko <ohrimenkoivan57@gmail.com>
2024-02-27 23:57:35 +0300

    cycle

---
+++ cycleTest/cycle.js
@@ -0,0 +1,26 @@
+function getDayName(day){
+    day = day - 1;                 //Отнимаем единицу от первого дня т. к. массив нумеруется 0
+    let days = [
+        "Mon",
+        "Tues",
+        "Wend",
+        "Thurs",
+        "Fri",
+        "Sat",
+        "Sun"
+    ];
+    if(days[day]){
+        return days[day];
+    } else {
+        throw "DayNo_idi_rabotay";
+    }
+}
+for(let i = 1; i<=8; i++){
+    try {
+        dayName = getDayName(i);      //Вызываем функцию с некорректным аргументом
+    } catch(error){
+        dayName = "unknown";
+        console.log(error)
+    }
+    console.log (dayName);
+}
